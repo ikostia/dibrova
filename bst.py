83be8f93abf9c87ce02514d1d29fa81334cce087
Kostia Balytskyi <kostia.balitsky@gmail.com>
2018-04-15 16:40:15 +0100

    bst: add naive bst implementations in python

---
+++ bst.py
@@ -0,0 +1,142 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+class BstNode(object):
+    """Binary search tree node"""
+
+    def __init__(self, data=None, parent=None):
+        self.data = data
+        self.parent = parent
+        self.left_child = None
+        self.right_child = None
+    
+    def isleftchild(self):
+        """Check if the node is the left child of its parent"""
+        if self.parent is None:
+            return False
+        return self == self.parent.left_child
+
+    def isrightchild(self):
+        """Check if the node is the right child of its parent"""
+        if self.parent is None:
+            return False
+        return self == self.parent.right_child
+
+    def isroot(self):
+        """Check if the node is the root of the BST"""
+        return self.parent is None
+
+    def __repr__(self):
+        return '<%r>' % self.data
+
+class BstIterator(object):
+    """Generic BST iterator class"""
+
+    def __init__(self, tree, return_nodes=False):
+        self.start = True
+        self.curr = None
+        self.tree = tree
+        self.return_nodes = return_nodes
+
+    def __iter__(self):
+        return self
+
+    def next_impl(self):
+        """Get next item for iterations >1
+        Must change self.curr"""
+        raise NotImplementedError("Method needs to be defined in real iterators")
+    
+    def first_impl(self):
+        """Get first item in the iteration
+        
+        If this method is called, the tree is guaranteed to be not empty"""
+        raise NotImplementedError("Method needs to be defined in real iterators")
+
+    def next(self):
+        if self.tree.root is None:
+            raise StopIteration()
+        if self.start:
+            self.start = False
+            self.first_impl()
+        else:
+            self.next_impl()
+        return self.curr if self.return_nodes else self.curr.data
+
+class BstInorderIterator(BstIterator):
+    """BstIterator for the in-order traversal"""
+    def first_impl(self):
+        self.curr = self.tree.min()
+
+    def next_impl(self):
+        node = self.curr
+        if node.right_child is not None:
+            node = node.right_child
+            while node.left_child is not None:
+                node = node.left_child
+            self.curr = node
+            return
+        # self.right_child is None
+        while node.isrightchild():
+            node = node.parent
+        if node.isroot():
+            raise StopIteration()
+        # now node is guaranteed to be a left_child
+        self.curr = node.parent
+
+class BstPreorderIterator(BstIterator):
+    """BstIterator for the pre-order traversal"""
+    def first_impl(self):
+        self.curr = self.tree.root
+
+    def next_impl(self):
+        node = self.curr
+        if node.left_child is not None:
+            self.curr = node.left_child
+            return
+        prev = None
+        while node and (node.right_child is None or node.right_child is prev):
+            prev = node
+            node = node.parent
+        if node is None:
+            raise StopIteration()
+        self.curr = node.right_child
+
+class BST(object):
+    """Basic binary search tree"""
+    def __init__(self, root=None):
+        self.root = root
+
+    def insert(self, data):
+        """Naive implementation of BST insert"""
+        if self.root is None:
+            self.root = BstNode(data=data)
+            return self.root
+
+        parent = None
+        node = self.root
+        while node != None:
+            parent = node
+            node = node.left_child if node.data > data else node.right_child
+        node = BstNode(data=data, parent=parent)
+        if node.data < parent.data:
+            parent.left_child = node
+        else:
+            parent.right_child = node
+
+    def min(self):
+        """Get the smallest element in the BST"""
+        node = self.root
+        while node.left_child is not None:
+            node = node.left_child
+        return node
+
+    def iter_inorder(self):
+        """Get the in-order traversal iterator for the BST"""
+        return BstInorderIterator(self)
+    
+    def iter_preorder(self):
+        """Get the pre-order traversal iterator for the BST"""
+        return BstPreorderIterator(self)
+
+    def __iter__(self):
+        return self.iter_inorder()
---
+++ tests.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+import unittest
+
+import bst as bstmod
+
+class TestBasicBST(unittest.TestCase):
+    def test_insert(self):
+        bst = bstmod.BST()
+        self.assertIsNone(bst.root)
+        bst.insert(8)
+        self.assertEqual(bst.root.data, 8)
+        bst.insert(4)
+        self.assertEqual(bst.root.left_child.data, 4)
+        self.assertIsNone(bst.root.right_child)
+        bst.insert(6)
+        self.assertEqual(bst.root.left_child.right_child.data, 6)
+        bst.insert(12)
+        self.assertEqual(bst.root.right_child.data, 12)
+
+    def build(self, lst):
+        bst = bstmod.BST()
+        for item in lst:
+            bst.insert(item)
+        return bst
+    
+    def ale(self, tolist, lst):
+        self.assertListEqual(list(tolist), lst)
+
+    def test_inorder(self):
+        # in-order is the default traversal
+        self.assertListEqual(list(self.build([1, 2, 3])), [1, 2, 3])
+
+        def ale(lst1, lst2):
+            self.assertListEqual(list(self.build(lst1).iter_inorder()), lst2)
+        for lst in [[0], [2, 1, 3], [1, 1, 2], [1, 2], [2, 1], [1, 1],
+                    [8, 4, 6, 12, 10, 13, 9], []]:
+            ale(lst, sorted(lst))
+
+    def test_preorder(self):
+        cases = [
+            ([1], [1]),
+            ([1,2], [1, 2]),
+            ([2, 1], [2, 1]),
+            ([2, 1, 3], [2, 1, 3]),
+            ([2, 3, 1], [2, 1, 3]),
+            ([12, 17, 8, 11, 13, 20, 6, 9, 15],
+             [12, 8, 6, 11, 9, 17, 13, 15, 20]),
+        ]
+        for totree, order in cases:
+            self.ale(self.build(totree).iter_preorder(), order)
+
+if __name__ == "__main__":
+    unittest.main()
