1be8ccfd6c34e5c4cd2865cb02d2895fb18b11f7
Kostia Balytskyi <kostia.balitsky@gmail.com>
2018-07-13 14:12:56 +0100

    [rs] dsu: add a DSU with parent compression implementation

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -2,26 +2,29 @@ trait IntDSU {
     /// Create a new disjoint set union structure
     fn new(n: usize) -> Self;
 
+    /// Find the leader of the set to which i belongs
+    fn find_leader(&mut self, i: usize) -> usize;
+
+    /// Make l the leader of the set to which i belongs
+    /// (l is expected to not belong to i's set at first)
+    fn set_leader(&mut self, i: usize, l: usize);
+
     /// Check if two elements belong to the same set
-    fn is_same_set(&mut self, i: usize, j: usize) -> bool;
+    fn is_same_set(&mut self, i: usize, j: usize) -> bool {
+        self.find_leader(i) == self.find_leader(j)
+    }
 
     /// Merge sets containing i and j into a signle set
-    fn join(&mut self, i: usize, j: usize);
+    fn join(&mut self, i: usize, j: usize) {
+        let il = self.find_leader(i);
+        let jl = self.find_leader(j);
+        self.set_leader(il, jl);
+    }
 }
 
 #[derive(Debug)]
 struct BaseDSU {
     parent: Vec<usize>
-}
-
-impl BaseDSU {
-    fn find_leader(&self, i: usize) -> usize {
-        let mut i = i;
-        while i != self.parent[i] {
-            i = self.parent[i];
-        }
-        i
-    }
 }
 
 impl IntDSU for BaseDSU {
@@ -33,14 +36,48 @@ impl IntDSU for BaseDSU {
         BaseDSU { parent: v }
     }
 
-    fn is_same_set(&mut self, i: usize, j: usize) -> bool {
-        self.find_leader(i) == self.find_leader(j)
+    fn find_leader(&mut self, i: usize) -> usize {
+        let mut i = i;
+        while i != self.parent[i] {
+            i = self.parent[i];
+        }
+        i
     }
 
-    fn join(&mut self, i: usize, j: usize) {
-        let il = self.find_leader(i);
-        let jl = self.find_leader(j);
-        self.parent[il] = jl;
+    fn set_leader(&mut self, i: usize, l: usize) {
+        self.parent[i] = l;
+    }
+}
+
+#[derive(Debug)]
+struct DsuWithPathCompression {
+    parent: Vec<usize>
+}
+
+impl IntDSU for DsuWithPathCompression {
+    fn new(n: usize) -> Self {
+        let mut v = Vec::with_capacity(n);
+        for i in 0..n {
+            v.push(i)
+        }
+        DsuWithPathCompression { parent: v }
+    }
+
+    fn find_leader(&mut self, i: usize) -> usize {
+        let mut i = i;
+        let mut path: Vec<usize> = vec![];
+        while i != self.parent[i] {
+            path.push(i);
+            i = self.parent[i];
+        }
+        for element in path {
+            self.parent[element] = i;
+        }
+        i
+    }
+
+    fn set_leader(&mut self, i: usize, l: usize) {
+        self.parent[i] = l;
     }
 }
 
@@ -60,5 +97,11 @@ mod tests {
     fn test_basic_operations_for_base_dsu() {
         let mut base_dsu = BaseDSU::new(10);
         test_basic_dsu_operations(&mut base_dsu);
+    }
+
+    #[test]
+    fn test_basic_operations_for_dsu_with_parent_compression() {
+        let mut dsu_with_parent_compression = DsuWithPathCompression::new(10);
+        test_basic_dsu_operations(&mut dsu_with_parent_compression);
     }
 }
