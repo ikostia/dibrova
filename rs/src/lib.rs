cfecb2389ca7e0c4f73d75e910f879c81d48d367
Kostia Balytskyi <kostia.balitsky@gmail.com>
2018-07-13 19:10:36 +0100

    [rs] dsu: add a generic unoptimized forest-based DSU implementation

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,25 +1,35 @@
 use std::hash::Hash;
-use std::cmp::PartialEq;
+use std::cmp::Eq;
+use std::cell::RefCell;
+use std::collections::{HashMap};
 
 /// Generic trait to represent the Disjoint Set Union structure
-trait DSU<T: Hash + PartialEq + Copy> {
+trait DSU<'a, T: Hash + Eq + 'a> {
     /// Return true if elements i and j are in the same set
-    fn is_same_set(&self, i: T, j: T) -> bool;
+    fn is_same_set(&self, i: &'a T, j: &'a T) -> bool;
 
     /// Merge sets containing elements i and j
-    fn join(&mut self, i: T, j: T);
+    /// Any implementation would need to make sure that
+    /// join utilizes the interior mutability
+    fn join(&self, i: &'a T, j: &'a T);
+
+    /// Inserts an element into its own set
+    fn insert(&mut self, el: &'a T);
 }
 
-/// A trait to represent the DSU, implemented as a tree
-trait TreeDsu<T: Hash + PartialEq + Copy> {
+/// A trait to represent a forest
+trait Forest<'a, T: Hash + Eq + 'a> {
     /// Return the parent node of node i in the tree
-    fn get_parent(&self, i: T) -> T;
+    /// Returning the same node symbolizes the tree root.
+    fn get_parent(&self, i: &'a T) -> &'a T;
 
     /// Set the parent of node i in the tree to be node p
-    fn set_parent(&mut self, i: T, p: T);
+    /// Any implementation would need to make sure that
+    /// set_parent utilizes the interior mutability
+    fn set_parent(&self, i: &'a T, p: &'a T);
 
-    /// Find the leader of the set, containing i
-    fn find_leader<'a>(&'a self, i: T) -> T {
+    /// Find the root of the tree, containing i
+    fn find_root(&self, i: &'a T) -> &'a T {
         let mut i = i;
         let mut j = self.get_parent(i);
         while j != i {
@@ -28,48 +38,59 @@ trait TreeDsu<T: Hash + PartialEq + Copy> {
         }
         i
     }
+
+    /// Create a new tree with el as its root
+    fn new_tree_from_root(&mut self, el: &'a T);
 }
 
-impl<T, TD> DSU<T> for TD
+/// Any Forest is also a DSU
+impl<'a, T, F> DSU<'a, T> for F
 where
-    T: Hash + PartialEq + Copy,
-    TD: TreeDsu<T>
+    T: Hash + Eq + 'a,
+    F: Forest<'a, T>
 {
-    /// Return true if i and j belong to the same set
-    fn is_same_set(&self, i: T, j: T) -> bool {
-        self.find_leader(i) == self.find_leader(j)
+    fn is_same_set(&self, i: &'a T, j: &'a T) -> bool {
+        self.find_root(i) == self.find_root(j)
     }
 
-    /// Merge sets containing elements i and j
-    fn join(&mut self, i: T, j: T) {
-        let il = self.find_leader(i);
-        let jl = self.find_leader(j);
+    fn join(&self, i: &'a T, j: &'a T) {
+        let il = self.find_root(i);
+        let jl = self.find_root(j);
         self.set_parent(il, jl);
+    }
+
+    fn insert(&mut self, el: &'a T) {
+        self.new_tree_from_root(el);
     }
 }
 
 #[derive(Debug)]
-pub struct UsizeDSU {
-    parent: Vec<usize>
+pub struct ForestDsu<'a, T: 'a + Hash + Eq>
+{
+    parents: RefCell<HashMap<&'a T, &'a T>>
 }
 
-impl UsizeDSU {
-    pub fn new(n: usize) -> Self {
-        let mut v = Vec::with_capacity(n);
-        for i in 0..n {
-            v.push(i)
-        }
-        Self { parent: v }
+impl<'a, T: 'a + Hash + Eq> ForestDsu<'a, T>
+{
+    pub fn new() -> Self {
+        Self { parents: RefCell::new(HashMap::new()) }
     }
 }
 
-impl TreeDsu<usize> for UsizeDSU {
-    fn get_parent(&self, i: usize) -> usize {
-        self.parent[i]
+impl<'a, T> Forest<'a, T> for ForestDsu<'a, T>
+where
+    T: 'a + Hash + Eq
+{
+    fn new_tree_from_root(&mut self, el: &'a T) {
+        (*self.parents.borrow_mut()).insert(el, el);
     }
 
-    fn set_parent(&mut self, i: usize, p: usize) {
-        self.parent[i] = p;
+    fn get_parent(&self, i: &'a T) -> &'a T {
+        self.parents.borrow()[i]
+    }
+
+    fn set_parent(&self, i: &'a T, p: &'a T) {
+        (*self.parents.borrow_mut()).insert(i, p);
     }
 }
 
@@ -77,17 +98,24 @@ impl TreeDsu<usize> for UsizeDSU {
 mod tests {
     use super::*;
 
-    fn test_basic_dsu_operations<T: DSU<usize>>(dsu: &mut T) {
-        assert_eq!(dsu.is_same_set(1, 2), false);
-        dsu.join(1, 2);
-        assert_eq!(dsu.is_same_set(1, 2), true);
-        dsu.join(1, 3);
-        assert_eq!(dsu.is_same_set(3, 2), true);
+    fn test_basic_dsu_operations<'a, T, D>(dsu: &mut D, elements: &'a [T]) 
+    where
+        T: 'a + Hash + Eq,
+        D: DSU<'a, T>
+    {
+        dsu.insert(&elements[0]);
+        dsu.insert(&elements[1]);
+        dsu.insert(&elements[2]);
+        assert_eq!(dsu.is_same_set(&elements[0], &elements[1]), false);
+        dsu.join(&elements[0], &elements[1]);
+        assert_eq!(dsu.is_same_set(&elements[0], &elements[1]), true);
+        dsu.join(&elements[0], &elements[2]);
+        assert_eq!(dsu.is_same_set(&elements[2], &elements[1]), true);
     }
 
     #[test]
-    fn test_basic_operations_for_base_dsu() {
-        let mut base_dsu = UsizeDSU::new(10);
-        test_basic_dsu_operations(&mut base_dsu);
+    fn test_basic_operations_for_base_dsu<'a>() {
+        let mut base_dsu: ForestDsu<'a, usize> = ForestDsu::new();
+        test_basic_dsu_operations(&mut base_dsu, &[1, 2, 3]);
     }
 }
