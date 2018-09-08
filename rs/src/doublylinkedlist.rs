65e72dfe08552398ce21287227691c0e85f92d06
Kostia Balytskyi <kostia.balitsky@gmail.com>
2018-09-08 22:25:21 +0100

    [rs] doublylinkedlist: add drop implementation

--- rs/src/doublylinkedlist.rs
+++ rs/src/doublylinkedlist.rs
@@ -100,6 +100,12 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {};
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
